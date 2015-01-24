2929ba4825223c68f691f25ad5b05f0e78b3cddf
seq218 <seq218@lehigh.edu>
2015-01-24 17:41:30 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Sarah Qureshi
+//Demo
+//Welcome Class Java Program 
+//
+//  first compile the program 
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+
+//add main method
+    public static void main(String [] args) {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("/\\ /\\ /\\ /\\ /\\ /\\ ");
+        System.out.println("<-S--E--Q--2--1--8->");
+        System.out.println("\\/ \\/ \\/ \\/ \\/ \\/");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("I am Sarah Qureshi.  I'm from Englewood Cliffs, NJ.  I'm an IDEAS major.");
+    }
+}
