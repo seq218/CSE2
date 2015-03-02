4a993885b67b60438d92c770dbcba04ec5a748b2
seq218 <seq218@lehigh.edu>
2015-03-02 22:04:34 +0000

    hw06

---
+++ hw06/GetIntegers.java
@@ -0,0 +1,16 @@
+//Sarah Qureshi
+//CSE02-111
+//March 3, 2015
+//Purpose:
+    //Find the sum of five integers 
+
+import java.util.Scanner; //import scanner 
+
+public class GetIntegers{
+    public static void main(String args []){
+        Scanner myScanner; //declare scanner variable 
+        myScanner= new Scanner(System.in); //construct scanner 
+        System.out.println("Enter 5 non-negative integers"); //prompt user to input five integers
+        number1= myScanner.nextInt(); 
+    }
+}
---
+++ hw06/RunFactorial.java
@@ -0,0 +1,34 @@
+//Sarah Qureshi
+//CSE2-111
+//March 3, 2015
+//Purpose:
+    //Find the factorial of a number from  to 16
+    import java.util.Scanner; //import scanner 
+
+public class RunFactorial{
+    public static void main(String args[]){
+        Scanner myScanner= new Scanner(System.in); //declare and construct scanner 
+        int i=0; //initialize counter
+        System.out.println("Enter an integer between 9 and 16"); //prompt user to input number between 9 and 16
+    while(i<=100){
+        i++; //increment i
+        int integer=myScanner.nextInt(); //initialize integer 
+        if(integer>=9 && integer<=16 && integer%1==0){
+            int c=1; //initialize counter 
+            long factorial=1; //initialize factorial
+            while(c<=integer){
+                factorial=factorial*c; //factorial 
+                c++; //increment c
+                
+            }
+            System.out.println(integer+"!="+factorial); //print results
+            break; //break 
+            
+        }
+        else{
+            System.out.println("invalid input, try again"); //print invalid input
+        }
+    }
+        
+    }
+}
